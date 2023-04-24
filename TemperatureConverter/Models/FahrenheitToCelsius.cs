62ed1fa32bc89cf81e6ef61777729ee483532105
Emzzy241 <emzzyoluwole@gmail.com>
2023-04-24 08:55:56 +0100

    adding my FahrenheitToCelsius converter class-file

---
+++ TemperatureConverter/Models/FahrenheitToCelsius.cs
@@ -0,0 +1,24 @@
+// This is the class file for the conversion of Fahrenheit to Celsius Temperature
+
+using System;
+
+
+// a method that takes in a fahrenheit Temperature and converts to celsius temperature
+
+namespace TemperatureConverter.Models
+{
+
+
+    class FahrenheitToCelsius
+    {
+        // public string FahrenheitToCelsius()
+        // {
+
+        // }
+
+        // WRITING A PUBLIC METHOD THAT WILL ADD
+    }
+
+
+}
+
