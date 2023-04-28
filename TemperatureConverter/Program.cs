bfb3250c9064c6452efc4c79a4fa32b6bf0e03f0
Emzzy241 <emzzyoluwole@gmail.com>
2023-04-28 02:49:40 +0100

    writing a new UI method to confirm temperature value

--- TemperatureConverter/Program.cs
+++ TemperatureConverter/Program.cs
@@ -6,8 +6,10 @@ using System.Collections.Generic;
 
 
 using TemperatureConverter.Models;
-class Program{
-    static void Main(){
+class Program
+{
+    static void Main()
+    {
 
         // My appliction can perform 2 features; one is converting fahrenheit Temperature to celsius and vis a vis(vice-versa)
         // and the other is converting a temperature n Kelvin to celsius and vis a vis(vice-versa)
@@ -20,45 +22,92 @@ class Program{
 
         string userStarts = Console.ReadLine().ToUpper();
 
-        if(userStarts == "F")
+        if (userStarts == "F")
         {
 
         }
-        else if(userStarts == "K")
+        else if (userStarts == "K")
         {
-            Console.WriteLine("You selected the option for a conversion of Kelvin to Celsius");
-            Console.WriteLine();
-            Console.WriteLine("Please enter in the kelvin value");
-            string kelvinValue = Console.ReadLine();
+              Console.WriteLine("You selected the option for a conversion of Kelvin to Celsius");
+              Console.WriteLine();
 
-            // converting that string into a integer
-            int kelvinValueInt = int.Parse(kelvinValue);
+            Console.WriteLine("Would you like to proceed('YES' or 'NO')");
+            string userProceed = Console.ReadLine().ToUpper();
+
+            if(userProceed == "YES")
+            {
+                ConvertKelvinValueToCelsius();
+            }
+
+            // The first UI logic method to write will be on picking up a Kelvin temperature and converting it to a 
+            // celsius temperature 
+
+            static void ConvertKelvinValueToCelsius(KelvinToCelsius kel)
+            {
+
+              
+                Console.WriteLine();
+                Console.WriteLine("Please enter in the kelvin value");
+                string kelvinValue = Console.ReadLine();
+
+                // converting that string into a integer
+
+                int kelvinValueInt = int.Parse(kelvinValue);
+
+                Console.WriteLine(kelvinValueInt);
+
+
+                // the Convert To Temperature method here is from my Kelvin to tcelsius Temperature C# file
+
+
+                // if (kelvinValueInt < 0)
+                // {
+                //     throw new ArgumentException("Invalid temperature in kelvin");
+                // }
+
+                // else
+                // {
+                //     string result = kel.KelConverterMethod(kelvinValueInt);
+
+                //     int kelvinTemp = -273;
+                //     // int celsiusValue = kelvinStandardValue - 273;
+                //     // return kelvinTemp;
+                // }
+
+                // }
+
+                // public int KelConverterMethod(int kelvinStandardValue)
+                // {
+                //     // NOTE: a C# method must always return something otherwise it gives the "not all code paths return a value error"
+
+                //     if(kelvinStandardValue < 0)
+                //     {
+                //         throw new ArgumentException("Invalid temperature in kelvin");
+                //     }
+
+                //     else{
+                //         int kelvinTemp = - 273;
+                //         // int celsiusValue = kelvinStandardValue - 273;
+                //         return kelvinTemp;
+                //     }
+                // }
 
 
 
-        // The first UI logic method to write will be on picking up a Kelvin temperature and converting it to a 
-        // celsius temperature 
 
-        static void ConvertKelvinValueToCelsius(KelvinToCelsius kel)
-        {
-            // the Convert To Temperature method here is from my Kelvin to tcelsius Temperature C# file
+            }
 
-            string result = kel.KelConverterMethod();
+
+
+
 
         }
 
-
-
-
-        }
-        else{
+        else
+        {
             Console.WriteLine("I am sorry, I didn't get that; Please try again");
             Main();
         }
-
-
-
-        
 
     }
 }
